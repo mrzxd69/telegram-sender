a6247803e2f23472811c47be79d651baf4f20b57
mrzxd69 <mrzxd69@gmail.com>
2023-02-03 17:09:00 +1200

    finish

--- function/sendMessage.js
+++ function/sendMessage.js
@@ -1,6 +1,6 @@
 import config from '../config.js'
 
-export default async (ip, number, email, name) => {
+export default async (ip, name, email, number) => {
     const message = `✅ Новая%20заявка%21%0A%0Aip:%20${ip}%0AИмя:%20${name}%0AНомер:%20${number}%0AПочта:%20${email}`
 
     let data = await fetch(config.apiUrl + config.botToken + '/sendMessage?chat_id=' + config.chatId + '&text=' + message)
